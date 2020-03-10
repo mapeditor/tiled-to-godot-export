f854773a15a69d7a8679038306eb43cc4a79b62c
Mihail Ilinov <mihail@ilinov.eu>
2020-03-11 00:57:00 +0200

    Refactor to class for readability

--- export_to_godot_tileset.js
+++ export_to_godot_tileset.js
@@ -1,42 +1,84 @@
-const GodotExporter = (function () {
+/*global tiled, TextFile */
+class GodotExporter {
 
-    const Constructor = function (tileset, fileName) {
+    constructor(tileset, fileName) {
         this.tileset = tileset;
         this.fileName = fileName;
+        // noinspection JSUnresolvedFunction
         this.projectRoot = this.tileset.property("projectRoot");
         this.spriteImagePath = this.tileset.image.replace(this.projectRoot, "");
+        this.shapesResources = "";
+        this.shapes = "";
     };
 
-    Constructor.prototype.write = function () {
+    write() {
 
-        log("--- tileset start ---");
-        logf(this.tileset.tiles[0].objectGroup);
-        log(this.tileset.tileCount);
-        log("--- tileset end ---");
+        // Exports collision shapes ... for now
+        this.iterateTiles();
 
-        this.tileset.tiles.forEach((tile, index) => {
-            // log("tile id: " + tile.id);
-            // if(tile.objectGroup.objects.length > 0){
-            //     // tile.objectGroup.objects.forEach((mapObject) => {
-            //     //     logk(mapObject.polygon);
-            //     // });
-            // }
-
-            //     let shapesResources += customTilesetFormat.getShapesResourcesTemplate(tile.id);
-        });
-
-        this.shapesResources = this.getShapesResourcesTemplate();
-        this.shapes = this.getShapesTemplate();
-
-        let tilesetTemplate = this.getTilesetTemplate();
-
+        // noinspection JSUnresolvedVariable
         const file = new TextFile(this.fileName, TextFile.WriteOnly);
+        let tilesetTemplate = this.getTilesetTemplate();
         file.write(tilesetTemplate);
         file.commit();
         // file.close(); //not neede anymore since tiled 1.3.3
-    };
+    }
 
-    Constructor.prototype.getTilesetTemplate = function () {
+    iterateTiles() {
+
+        let autotileCoordinates = {x: 0, y: 0};
+
+        // noinspection JSUnresolvedVariable
+        let tiles = this.tileset.tiles;
+
+        for (let index = 0; index < tiles.length; index++) {
+            let tile = tiles[index];
+
+            // noinspection JSUnresolvedVariable
+            if (tile.objectGroup !== null) {
+
+                // noinspection JSUnresolvedVariable
+                if (tile.objectGroup.objects.length > 0) {
+
+                    // noinspection JSUnresolvedVariable
+                    for (let oIndex = 0; oIndex < tile.objectGroup.objects.length; oIndex++) {
+
+                        // noinspection JSUnresolvedVariable
+                        let object = tile.objectGroup.objects[oIndex];
+
+                        //TODO: add occlusions, navigation
+                        this.exportCollisions(object, tile, autotileCoordinates);
+                    }
+                }
+            }
+
+            autotileCoordinates.x += 1;
+            if ((index) < this.tileset.columns) {
+                autotileCoordinates.y += 1;
+            }
+
+        }
+
+        this.shapes = this.shapes.replace(/,\s*$/, "");
+    }
+
+    exportCollisions(object, tile, autotileCoordinates) {
+        // noinspection JSUnresolvedVariable
+        if (object.polygon) {
+            this.shapesResources += this.getCollisionShapePolygon(tile.id, object.polygon);
+        } else if (object.width !== 0 && object.height !== 0) {
+            this.shapesResources += this.getCollisionShapeRectangle(tile.id, object.width);
+        }
+
+        this.shapes += this.getShapesTemplate(
+            autotileCoordinates,
+            false,
+            tile.id
+        );
+    }
+
+    getTilesetTemplate() {
+        // noinspection JSUnresolvedVariable
         return `[gd_resource type="TileSet" load_steps=3 format=2]
 
 [ext_resource path="res://${this.spriteImagePath}" type="Texture" id=1]
@@ -61,25 +103,38 @@ ${this.shapesResources}
 0/shapes = [ ${this.shapes} ]
 0/z_index = 0
 `;
-    };
-    Constructor.prototype.getShapesResourcesTemplate = function (id) {
-        return `[sub_resource type="ConvexPolygonShape2D" id=1]
-points = PoolVector2Array( 0, 0, 16, 0, 16, 16, 0, 16 )
-`;
-    };
-    Constructor.prototype.getShapesTemplate = function () {
-        return `{
-"autotile_coord": Vector2( 0, 0 ),
-"one_way": false,
-"one_way_margin": 1.0,
-"shape": SubResource( 1 ),
-"shape_transform": Transform2D( 1, 0, 0, 1, 0, 0 )
-}
-`;
-    };
+    }
 
-    return Constructor;
-})();
+    getShapesTemplate(coordinates, isOneWay, shapeID) {
+        return `{
+"autotile_coord": Vector2( ${coordinates} ),
+"one_way": ${isOneWay},
+"one_way_margin": 1.0,
+"shape": SubResource( ${shapeID} ),
+"shape_transform": Transform2D( 1, 0, 0, 1, 0, 0 )
+}, 
+`;
+    }
+
+    getCollisionShapePolygon(id, polygon) {
+        let coordinateString = "";
+        polygon.forEach((coordinate) => {
+            coordinateString += coordinate.x + ", " + coordinate.y + ", ";
+        });
+        // Remove trailing commas and blank
+        coordinateString = coordinateString.replace(/,\s*$/, "");
+        return `[sub_resource type="ConvexPolygonShape2D" id=${id}]
+points = PoolVector2Array( ${coordinateString} )
+`;
+    }
+
+    getCollisionShapeRectangle(id, tileSize) {
+        return `[sub_resource type="ConvexPolygonShape2D" id=${id}]
+points = PoolVector2Array( 0, 0, ${tileSize}, 0, ${tileSize}, ${tileSize}, 0, ${tileSize} )
+`;
+    }
+
+}
 
 const customTilesetFormat = {
     name: "Godot Tileset format",
@@ -91,4 +146,5 @@ const customTilesetFormat = {
     }
 };
 
+// noinspection JSUnresolvedFunction
 tiled.registerTilesetFormat("Godot", customTilesetFormat);
